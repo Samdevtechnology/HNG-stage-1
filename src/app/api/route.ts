4b363000cf4c122c8572acce10df1d69780c5140
Samdevtechnology <Samdevtechnology@gmail.com>
2025-02-01 06:23:31 +0100

    Refactor API route to update variable names for clarity

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,12 +1,12 @@
 import { NextResponse } from "next/server";
 
 const email = "samdevtechnology@gmail.com";
-const timestamp = new Date().toISOString();
-const gitUrl = "https://github.com/Samdevtechnology/HNG-stage-1";
+const current_datetime = new Date().toISOString();
+const github_url = "https://github.com/Samdevtechnology/HNG-stage-1";
 
 export async function GET() {
   return NextResponse.json(
-    { email, timestamp, gitUrl },
+    { email, current_datetime, github_url },
     {
       status: 200,
       headers: {
